fe98912ea6b13b9f4c09c78f5e73ee3a7504c10c
Hassanal <Hassanal@Hassanals-Laptop>
2021-11-21 15:22:14 +0800

    This is first commit for question 2

--- LabReport/src/main/java/LabReport/Question2.java
+++ LabReport/src/main/java/LabReport/Question2.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package LabReport1;
+package LabReport;
 
 /**
  *
  * @author Hassanal
  */
-public class Q2 {
+public class Question2 {
 
     /**
      * @param args the command line arguments
