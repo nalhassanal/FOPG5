256a990bc8815e2a6d3914a22bca757570427ee5
Hassanal <u2102848@siswa.um.edu.my>
2022-01-02 17:26:42 +0800

    fourth commit loginpage

--- MayaNo2/src/main/java/First/LoginPage.java
+++ MayaNo2/src/main/java/First/LoginPage.java
@@ -1,4 +1,5 @@
 package First;
+
 /**
  *
  * @author Hassanal
@@ -13,14 +14,16 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class LoginPage {
-    public static void staffRegister(){
+
+    public static void staffRegister() {
         //this method will ask users input for information
         //this method creates two files, staff and staffCredentials
         //student stores all staff information
         //staffCredentials is used explicitly by student login 
         Scanner input = new Scanner(System.in);
-        String mail, username, password, fullname ,staff;
+        String mail, username, password, fullname, staff;
         // asking user credentials
         System.out.println("Please enter your Full Name");
         fullname = input.nextLine();
@@ -30,49 +33,49 @@ public class LoginPage {
         username = input.nextLine();
         System.out.println("Please enter your password");
         password = input.nextLine();
-        
+
         //making all credentials into one string
-        staff = username + " "+mail + " " +fullname +" "+password ;
-        
-        try{
+        staff = username + " " + mail + " " + fullname + " " + password;
+
+        try {
             String filename = "staff";
             // creates new file if not there
             // if file exists it will do nothing
             File file = new File(filename);
             // appends existing file
-            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file,true));
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file, true));
             //adds the string staff into the file
             outputStream.println(staff);
             //closes the file connection
             outputStream.close();
-        }catch(IOException ex){
-            System.out.println("IO Error " +ex.getMessage());
-        }    
-        
-        try{
+        } catch (IOException ex) {
+            System.out.println("IO Error " + ex.getMessage());
+        }
+
+        try {
             String filename = "staffCredentials";
             // creates new file if not there
             // if file exists it will do nothing
             File file = new File(filename);
             // appends existing file
-            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file,true));
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file, true));
             //adds the string staff into the file
             outputStream.println(username);
             outputStream.println(password);
             //closes the file connection
             outputStream.close();
-        }catch(IOException ex){
-            System.out.println("IO Error " +ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("IO Error " + ex.getMessage());
         }
     }
 
-    public static void studentRegister(){
+    public static void studentRegister() {
         //this method will ask users input for information
         //this method creates two files, student and studentCredentials
         //student stores all student information
         //studentCredentials is used explicitly by student login 
         Scanner input = new Scanner(System.in);
-        String mail, matrixNum, password, fullname ,student;
+        String mail, matrixNum, password, fullname, student;
         // asking user credentials
         System.out.println("Please enter your Full Name");
         fullname = input.nextLine();
@@ -82,127 +85,129 @@ public class LoginPage {
         matrixNum = input.nextLine();
         System.out.println("Please enter your password");
         password = input.nextLine();
-        
+
         //making all credentials into one string
-        student = matrixNum + " " +mail + " " +fullname +" " +password ;
-        
-        try{
+        student = matrixNum + " " + mail + " " + fullname + " " + password;
+
+        try {
             String filename = "student";
             // creates new file if not there
             // if file exists it will do nothing
             File file = new File(filename);
             // appends existing file
-            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file,true));
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file, true));
             //adds the string studentinto the file
             outputStream.println(student);
             //closes the file connection
             outputStream.close();
-        }catch(IOException ex){
-            System.out.println("IO Error " +ex.getMessage());
-        } 
-        
-        try{
+        } catch (IOException ex) {
+            System.out.println("IO Error " + ex.getMessage());
+        }
+
+        try {
             String filename = "studentCredentials";
             // creates new file if not there
             // if file exists it will do nothing
             File file = new File(filename);
             // appends existing file
-            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file,true));
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(file, true));
             //adds the credentials into the file
             outputStream.println(matrixNum);
             outputStream.println(password);
             //closes the file connection
             outputStream.close();
-        }catch(IOException ex){
-            System.out.println("IO Error " +ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("IO Error " + ex.getMessage());
         }
     }
-    
-    public static String staffLogin(){
+
+    public static String staffLogin() {
         // this method will read from staffCredential file 
         // will ask for staff input for username and password
         // and will check every element in the file
         // to compare with given input
         Scanner in = new Scanner(System.in);
         String userInput, passInput;
-        int count=0;
+        int count = 0;
         System.out.println("Please enter your username and password");
         userInput = in.nextLine();
         passInput = in.nextLine();
-        
+
         try {
             String filename = "staffCredentials";
-            
+            // to read contents of file
             Scanner inputStream = new Scanner(new FileInputStream(filename));
-            
-            while (inputStream.hasNextLine()){
-                if(userInput.equals(inputStream.nextLine())){
+
+            while (inputStream.hasNextLine()) {
+                if (userInput.equals(inputStream.nextLine())) {//line 1
                     count = 1;
-                    if(passInput.equals(inputStream.nextLine()))
+                    if (passInput.equals(inputStream.nextLine())) {// line 2
                         count = 2;
-                }
-                else{
-                    inputStream.nextLine();
+                    }
+                } else {
+                    inputStream.nextLine();// skip line 2
                     count = 0;
                 }
             }
-            
+
             inputStream.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(LoginPage.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex){
-            System.out.println("IO error "+ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("IO error " + ex.getMessage());
         }
-        
-        if (count == 2)
+
+        if (count == 2) {
             return "Login successful";
-        else if (count ==1)
+        } else if (count == 1) {
             return "Login unsuccessful\nwrong password";
-        else
+        } else {
             return "User is not registered";
+        }
     }
-    
-    public static String studentLogin(){
+
+    public static String studentLogin() {
         // this method will read from studentCredential file 
         // will ask for student input for matrix num and password
         // and will check every element in the file
         // to compare with given input
         Scanner in = new Scanner(System.in);
         String userInput, passInput;
-        int count=0;
+        int count = 0;
         System.out.println("Please enter your matrix number and password");
         userInput = in.nextLine();
         passInput = in.nextLine();
-        
+
         try {
             String filename = "studentCredentials";
-            
+
             Scanner inputStream = new Scanner(new FileInputStream(filename));
-            
-            while (inputStream.hasNextLine()){
-                if(userInput.equals(inputStream.nextLine())){
+
+            while (inputStream.hasNextLine()) {
+                if (userInput.equals(inputStream.nextLine())) {
                     count = 1;
-                    if(passInput.equals(inputStream.nextLine()))
+                    if (passInput.equals(inputStream.nextLine())) {
                         count = 2;
-                }
-                else{
+                    }
+                } else {
                     inputStream.nextLine();
                     count = 0;
                 }
             }
-            
+
             inputStream.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(LoginPage.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex){
-            System.out.println("IO error "+ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("IO error " + ex.getMessage());
         }
-        
-        if (count == 2)
+
+        if (count == 2) {
             return "Login successful";
-        else if (count ==1)
+        } else if (count == 1) {
             return "Login unsuccessful\nwrong password";
-        else
+        } else {
             return "User is not registered";
+        }
     }
 }
