7f51fcd1d90c555a81d52c39a704ca9945167c36
Hassanal <u2102848@siswa.um.edu.my>
2022-01-03 10:39:02 +0800

    idk lah this staff page stuck at delete

--- MayaNo2/src/main/java/Staff/StaffPage.java
+++ MayaNo2/src/main/java/Staff/StaffPage.java
@@ -1,8 +1,11 @@
 package Staff;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -22,23 +25,28 @@ public class StaffPage {
             System.out.println("1. Modify Modules");
             System.out.println("2. View Modules");
             System.out.println("3. View Students in your classes");
+            System.out.println("4. Exit");
             System.out.println("Enter the number you want >> ");
             menu = input.nextInt();
-            if ( menu == 1){
-                module();
-                System.out.println("end program");
-                break;
-            }
-            else if( menu == 2){
-
-            }
-            else if ( menu == 3){
-
-            }
-            else{
-                System.out.println("Invalid number, try again");
-                if ( count >= 3)
+            switch (menu) {
+                case 1:
+                    module();
+                    //staffPage(); // rename main as staffPage
+                    break;
+                case 2:
+                    view();
+                    break;
+                case 3:
+                    stuClass();
+                    break;
+                case 4:
                     keepGoing = false;
+                    break;
+                default:
+                    System.out.println("Invalid number, try again");
+                    if ( count >= 3)
+                        keepGoing = false;
+                    break;
             }
         }        
     }
@@ -51,17 +59,22 @@ public class StaffPage {
         System.out.println("1. Create a new module");
         System.out.println("2. Delete existing module");
         System.out.println("3. Edit a module");
+        System.out.println("4. Exit");
         System.out.println("Enter the number you want >> ");
         menu = input.nextInt();
         if ( menu == 1){
             String process = addModule();
             System.out.println(process);
+            module();
         }
         else if ( menu == 2){
-            
+            deleteModule();
         }
         else if ( menu == 3){
-            
+            editModule();
+        }
+        else if ( menu == 4){
+            //staffPage(); // rename main as staffPage
         }
         else{
             System.out.println("invalid number , try again");
@@ -76,23 +89,29 @@ public class StaffPage {
         String ret = "Unsuccessfull Module Addition";
         final String ACT1 = "Lecture", ACT2 = "Tutorial" , ACT3 = "Lab";
         System.out.println("Welcome to add module!");
+       
         // ask module code
         System.out.println("Please enter the module code > ");
         moduleCode = input.nextLine();
+       
         // ask module name
         System.out.println("Please enter the module name > ");
         moduleName = input.nextLine();
+        
         // ask the number of occurence
         System.out.println("Please enter the number of occurences > ");
         numofOcc = input.nextInt();
+        
         // ask the number of occurences
         System.out.println("Please enter the number of credits > ");
         credits = input.nextInt();
+       
         // ask the number of activities
         System.out.println("How many activities will this module have?\n( must less than or equal to 2 )");
         numAct = input.nextInt();
         int [] inACT = new int [numAct];
         String [] ACT = new String [numAct];
+        
         // ask the type of activities
         System.out.println("Please enter the activities type");
         System.out.println("1. Lecture\t2. Tutorial\t 3. Lab");
@@ -113,6 +132,7 @@ public class StaffPage {
                     break;
             }
         }
+        
         if ( numAct == 1){
             Activities = ACT[0];
         }
@@ -121,7 +141,7 @@ public class StaffPage {
         }
       
         try{
-            String filename = moduleName + " occurence.txt";
+            String filename = moduleCode + ".txt";
             File file = new File(filename);
             
             PrintWriter outputStream = new PrintWriter(new FileOutputStream(file,true));
@@ -134,6 +154,7 @@ public class StaffPage {
             outputStream.println(moduleCode);
             outputStream.println(moduleName);
             outputStream.println(numofOcc);
+            outputStream.flush();
             outputStream.close();
         } catch(IOException ex) {
             System.out.println("IO Error " +ex.getMessage());
@@ -152,7 +173,7 @@ public class StaffPage {
             outputStream.println(Activities);
             
             ret = "Module Added successfully";
-            
+            outputStream.flush();
             outputStream.close();            
             
         } catch(IOException ex){
@@ -162,7 +183,70 @@ public class StaffPage {
     }
     
     private static void deleteModule(){
+        input = new Scanner(System.in);
         
+        String delete; 
+        System.out.println("Please enter the module code that you want to delete");
+        delete = input.nextLine();
+        
+        String filename = "output.txt";
+        String filename1 = "allModules.txt";
+        String filename2 = "delete.txt";
+        try {
+            File file = new File(filename2);
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(filename2,true));
+            outputStream.println(delete);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException ex) {
+            System.out.println("IO error "+ex.getMessage());
+        }
+        
+        try{
+            File file = new File(filename);
+            File file1 = new File(filename1);
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(filename,true));
+
+            Scanner inputStream = new Scanner(new FileInputStream(filename1));
+            String line1 = inputStream.nextLine();
+            
+            while(inputStream.hasNextLine()){
+                Scanner inputStream1 = new Scanner(new FileInputStream(filename2));
+                String line2 = inputStream1.nextLine();
+                
+                while (inputStream1.hasNextLine()){
+                    boolean flag = false;
+                    if (line1.equals(line2)){
+                        flag = true;
+                        break;
+                    }
+                    else{
+                        inputStream.nextLine();
+                        inputStream.nextLine();
+                        inputStream.nextLine();
+                    }
+                    line2 = inputStream1.nextLine();
+                    if(!flag){
+                        outputStream.println(line1);
+                    }
+                }
+                line1 = inputStream.nextLine();
+            }
+            
+//            if(file1.renameTo(file))
+//                System.out.println("File succesffully renamed");
+//            else
+//                System.out.println("Failed");
+
+            outputStream.flush();
+            
+            //closing connections
+            inputStream.close();
+            outputStream.close();
+            System.out.println("File successful");
+        } catch(IOException ex){
+            System.out.println("IO error "+ex.getMessage());
+        }
     }
     
     private static void editModule(){
@@ -178,3 +262,87 @@ public class StaffPage {
     }
     
 }
+/*
+public static void deleteStudents() throws IOException, FileNotFoundException
+{
+    Scanner console = new Scanner(System.in);
+    String SY, date;
+    System.out.println("ENTER THE SCHOOL YEAR: SY: ");
+    SY = console.next();
+
+    int i = 0;
+
+    Scanner print = new Scanner(new FileReader("Students- SY " + SY + " " + ".txt"));
+    //display text file
+    while(print.hasNextLine())
+    {
+        String stud= print.nextLine();
+        System.out.println(stud);
+    }   
+
+    File inputFile = new File("Students- SY " + SY + " " + ".txt");
+    File tempFile = new File("Students- SY " + SY + " " + ".txt.bak");
+
+    BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+    BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+    String remove;
+    String currentLine;
+
+    System.out.println("ENTER STUDENT's ID NUMBER TO BE DELETED:  ");
+    remove = console.next();
+    while((currentLine = reader.readLine()) != null)
+    {
+        String trimmedLine = currentLine.trim();
+        if(!trimmedLine.startsWith(remove)) 
+        {
+            writer.write(String.format("%s%n",currentLine)); 
+
+        }
+    }  
+    //close reader/writer
+    writer.close();
+    reader.close();
+  //delete file
+    if(inputFile.delete())
+    {
+        tempFile.renameTo(inputFile);
+    }
+    else
+    {
+        System.out.println("FAIL");
+    }
+*/
+
+/*
+            while(line1 != null){
+                boolean flag = false;
+                
+               //BufferedReader br2 = new BufferedReader(new FileReader(filename2));
+                Scanner inputStream1 = new Scanner(new FileInputStream(filename2));
+                String line2 = inputStream1.nextLine();
+                
+                
+                if(line1.equals(line2)){
+                        flag = true;
+                }
+                
+                // if flag = false
+                // write line of filename1 to filename
+                if(!flag)
+                    outputStream.println(line1);
+                /*while (line2 != null){
+                    if(line1.equals(line2)){
+                        flag = true;
+                        break;
+                    }
+                    // update condition
+                    line2 = inputStream1.next();
+                }
+                
+                //update condition
+                line1 = inputStream.nextLine();
+                if(line1 == null)
+                    System.out.println("Successfully deleted");
+            }
+            */
