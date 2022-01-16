254c81c967624fb00045a7040521e6ec96276482
Hassanal <u2102848@siswa.um.edu.my>
2022-01-16 20:49:37 +0800

    student page

--- MayaNo2/src/main/java/First/Student.java
+++ MayaNo2/src/main/java/First/Student.java
@@ -17,13 +17,13 @@ public class Student {
     
     public void student(){
         Scanner sc = new Scanner(System.in);
-        /*
+        searchModule search = new searchModule();
         addmodule addmoduleobject = new addmodule();
         deletemodule deletemoduleobject = new deletemodule();
         viewregistered viewregisteredobject = new viewregistered();
-        timetabletest timetableobject = new timetabletest();
+        timetable timetableobject = new timetable();
         timetableupdater timetableupdaterobject = new timetableupdater();
-        */
+        
         String studentmatrix = "";
         try{
             String filename = "loggerStu.txt";
@@ -37,40 +37,74 @@ public class Student {
         int count = 0;
         while (keepGoing){
             
-            System.out.println();
+            System.out.println(); // tambahh selection search module
             System.out.println("Welcome to Student page!!!");
-            System.out.println("1)See all module\n2)add module\n3)delete module\n4)view course registered\n5)view timetable\n6)exit");
+            System.out.println("1)See all module\n2)add module\n3)delete module\n4)view course registered\n5)view timetable\n6)Search module\n7)exit");
             System.out.println("What you want to do:");
             int userinput = sc.nextInt();
 
             switch(userinput){
                 case 1:
-                    System.out.println("Add module");
-                    //addmoduleobject.addmodule();
+                    System.out.println();
+                    view();
+                    System.out.println();
                     break;
 
                 case 2:
-                    System.out.println("delete module");
-                    //deletemoduleobject.deletemodule(studentmatrix);
+                    System.out.println();
+                    boolean inkeepGoing = true;
+                    while (inkeepGoing){
+                        addmoduleobject.addmodule(studentmatrix);
+                        System.out.println("Do you want to continue adding module?\nyes\nno");
+                        String input = sc.nextLine();
+                        if ( input.toUpperCase().equals("NO"))
+                            inkeepGoing = false;
+                        else {
+                            System.out.println("Invalid input");
+                            inkeepGoing = false;
+                        }
+                        System.out.println();
+                    }    
                     break;
 
                 case 3:
-                    System.out.println("View Course Register");
-                    //viewregisteredobject.registered(studentmatrix);
+                    System.out.println();
+                    deletemoduleobject.deletemodule(studentmatrix);
+                    System.out.println();
                     break;
 
                 case 4:
-                    System.out.println("View Course Register");
-                    //viewregisteredobject.registered(studentmatrix);
+                    System.out.println();
+                    viewregisteredobject.registered(studentmatrix);
+                    System.out.println();
                     break;
 
                 case 5:
-                    System.out.println("Timetable");
-                    //timetableupdaterobject.updater(studentmatrix);
-                    //timetableobject.printtimetable(studentmatrix);
+                    System.out.println();
+                    timetableupdaterobject.updater(studentmatrix);
+                    timetableobject.printtimetable(studentmatrix);
+                    System.out.println();
                     break;
 
                 case 6:
+                    System.out.println();
+                    inkeepGoing = true;
+                    while(inkeepGoing){
+                        search.SearchModule();
+                        System.out.println("Do you want to continue searching?");
+                        System.out.println("Yes or No");
+                        String input = sc.nextLine();
+                        if ( input.toUpperCase().equals("NO"))
+                            inkeepGoing = false;
+                        else {
+                            System.out.println("Invalid input");
+                            inkeepGoing = false;
+                        }
+                        System.out.println();
+                    }
+                    break;
+                
+                case 7:
                     keepGoing = false;
                     System.out.println("Exiting Student page...");
                     System.out.println();
@@ -78,14 +112,60 @@ public class Student {
 
                 default:
                     count++;
+                    System.out.println();
                     System.out.println("Invalid number, try again");
                     if ( count >= 3){
                         keepGoing = false;
                         System.out.println("3 failed tries.\nForcefully exit...");
                     }
+                    System.out.println();
                     break;
             }
             
         }
      }
+    
+    // this method is to show all modules
+    private void view(){
+        String filename1 = "modulelist.txt" , filename2 = "Englishmodulelist.txt", filename3 = "Specializationmodulelist.txt";
+        File modulelist = new File (filename1);
+        File english = new File(filename2);
+        File special = new File(filename3);
+        
+        try{
+            Scanner inputStream = new Scanner ( new FileInputStream (modulelist));
+            System.out.println();
+            System.out.println("Main module list");
+            while(inputStream.hasNextLine()){
+                System.out.println(inputStream.nextLine());
+            }
+            inputStream.close();
+        } catch (FileNotFoundException ex){
+            System.out.println("file not found " +ex.getMessage());
+        }
+        
+        try{
+            Scanner inputStream = new Scanner ( new FileInputStream (english));
+            System.out.println();
+            System.out.println("English module list");
+            while(inputStream.hasNextLine()){
+                System.out.println(inputStream.nextLine());
+            }
+            inputStream.close();
+        } catch (FileNotFoundException ex){
+            System.out.println("file not found " +ex.getMessage());
+        }
+        
+        try{
+            Scanner inputStream = new Scanner ( new FileInputStream (special));
+            System.out.println();
+            System.out.println("Specialization module list");
+            while(inputStream.hasNextLine()){
+                System.out.println(inputStream.nextLine());
+            }
+            inputStream.close();
+        } catch (FileNotFoundException ex){
+            System.out.println("file not found " +ex.getMessage());
+        }
+    }
 }
