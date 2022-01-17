a41efca30c194be640b12bc0377e3abde7db894d
Hassanal <u2102848@siswa.um.edu.my>
2022-01-17 13:15:36 +0800

    some bug fixes

--- MayaNo2/src/main/java/First/popupBox.java
+++ MayaNo2/src/main/java/First/popupBox.java
@@ -11,7 +11,6 @@ public class popupBox{
     public popupBox() {
     }
     
-    
     public void infoBox(String infoMessage, String titleBar)
     {
         JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
