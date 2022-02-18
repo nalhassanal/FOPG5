c3c9a8271ed1bcf749b9b51330d2a319ff4ee6ad
Hassanal <u2102848@siswa.um.edu.my>
2022-02-18 21:35:22 +0800

    mysql code i guess

--- MayaNo2/src/test/java/database/db.java
+++ MayaNo2/src/test/java/database/db.java
@@ -1,5 +1,23 @@
 package database;
 
-public class db {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class db {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "Hassanal8122*_";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection is successful " +url);
+        } catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
